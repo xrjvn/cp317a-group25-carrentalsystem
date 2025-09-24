77e1a72732a7fe76ef2cf9ece57e247c1c2bf1bd
samoresk <samoreskovic@gmail.com>
2025-09-24 14:08:52 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+###Main Source Code File###
+#Expect main functions here in typical documentation format#
+#Eg. as follows#
 
+#@param s  = string type variable
+#@return result = string appended with an exclamation mark
+def exclaim():
+  s = input("Print a statement you wish to exclaim: ")
+  s = s + ("!")
+  print("Your new statement is ", s)
+
+
+exclaim()
