8a1f4f9771ec854a3d8b8511514233c273e20b6c
samoresk <samoreskovic@gmail.com>
2025-09-24 19:40:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-// This is the main entry point for our Car Rental System
+// This is the main entry point for our Car Rental System //
+// All Functions will be documented with clear headers //
 
 public class Main {
+    // Welcomes the user to the service and provides menu options// 
     public static void main(String[] args) {
         System.out.println("Hello, welcome to the Car Rental System!");
         System.out.println("This is a placeholder for the main application logic.");
