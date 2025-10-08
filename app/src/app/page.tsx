c7c8f7189aeca85548c88f47713e4b003c27282a
Khush Patel <khushpatel@Khushs-MacBook-Pro.local>
2025-10-08 15:41:10 -0400

    bg gradient changes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 flex items-center justify-center">
-      <main className="text-center">
-        <h1 className="text-8xl font-bold mb-6">Coming Soon</h1>
-        <p className="text-lg text-gray-500 dark:text-gray-400">Car Rental System</p>
-        <p className="text-sm text-gray-400 dark:text-gray-400">(Arjun, Connor, Hayden, Khush, Sam)</p>
+    <div className="min-h-screen p-8 flex items-center justify-center noisy-gradient">
+      <main className="text-center relative z-10">
+        <h1 className="text-8xl font-bold mb-6 text-white tracking-tighter">Coming Soon</h1>
+        <p className="text-lg text-white/90 ">Car Rental System</p>
+        <p className="text-sm text-white/70 ">(Arjun, Connor, Hayden, Khush, Sam)</p>
       </main>
     </div>
   );
